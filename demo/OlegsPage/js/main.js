a077ab3666021cfb22d4ed55dc39645fdf3e7141
Cameron Class <kamrankutumov@gmail.com>
2020-06-17 19:40:48 +0300

    Modal - To other pages

--- demo/OlegsPage/js/main.js
+++ demo/OlegsPage/js/main.js
@@ -29,55 +29,8 @@ $(document).ready(function () {
     }
   });
 
-  // Click Pay
-  $(".click_pay").click(function () {
-    $(".buy-modal").toggleClass("buy-modal_active", 1000);
-  });
-  $(".modal-pay-close").click(function () {
-    $(".buy-modal").removeClass("buy-modal_active");
-  });
-
-  $(".click_pay").click(function () {
-    $("html").addClass("lock-2 lock-3");
-    $("body").addClass("lock-2 lock-3");
-  });
-  $(".modal-pay-close").click(function () {
-    if ($("html").hasClass("lock-3")) {
-      $("html").removeClass("lock-3");
-      $("body").removeClass("lock-3");
-    } else {
-      $("html").removeClass("lock-4");
-      $("body").removeClass("lock-4");
-    }
-  });
-
-  //click pay html
-  $(".click_pay-html").click(function () {
-    $("html").addClass("lock-4");
-    $("body").addClass("lock-4");
-  });
-  $(".click_pay-html").click(function () {
-    $(".buy-modal").toggleClass("buy-modal_active", 1000);
-  });
 });
 
-$(document).ready(function ($) {
-  $(".popup-open").click(function (e) {
-    $(".popup-fade").addClass("popup-fade-active", 1000);
-    $("html").addClass("lock-2");
-    $("body").addClass("lock-2");
-    e.preventDefault();
-  });
-
-  $(".popup-fade").click(function (e) {
-    if ($(e.target).closest(".popup").length == 0) {
-      $(".popup-fade").removeClass("popup-fade-active");
-      $(".js-close-popup").removeClass("popup-fade-active");
-      $("html").removeClass("lock-2");
-      $("body").removeClass("lock-2");
-    }
-  });
-});
 
 // Slider
 
