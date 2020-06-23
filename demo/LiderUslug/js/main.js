dcf8f665d5d627d6928d30aa6dc53d4940cf7991
Cameron Class <kamrankutumov@gmail.com>
2020-06-23 11:50:25 +0300

    Lider Uslug Added 2

--- demo/LiderUslug/js/main.js
+++ demo/LiderUslug/js/main.js
@@ -5,3 +5,46 @@ $( function() {
     $spinner.fadeOut();
     $preloader.delay(350).fadeOut('slow');
 });
+
+
+
+
+$(document).ready(function(){
+
+  // Js Toogle
+  $(".js-tags-toogle").click(function(){
+    $("#js-tags-ul").toggleClass("h-auto");
+  });
+
+
+  // If more 8 Add Class
+  if ($('#js-tags-ul li').length > 9) {
+    $(".js-tags-toogle").addClass("display-block");
+   } else {
+    $(".js-tags-toogle").removeClass("display-block");
+  }
+
+  // Card filter
+  $(".js-card-filter").click(function(){
+    $(this).toggleClass("card-filter");
+  });
+
+  // Change Card View
+  $(".js-card-list").click(function(){
+    $(this).addClass("active");
+    $(".js-card-table").removeClass("active");
+    $(".catalog__card").addClass("catalog__card_list");
+  });
+  $(".js-card-table").click(function(){
+    $(this).addClass("active");
+    $(".js-card-list").removeClass("active");
+    $(".catalog__card").removeClass("catalog__card_list");
+  });
+
+  // Category toggleClass
+  // $(".js-category-open").click(function(e){
+  //   e.preventDefault();
+  //   $(".catalog-ul ul").toggleClass("display-block");
+  // });
+
+});
