22eda1b83c3884cae223aff1e5b57c5b61ea3e96
Cameron Class <kamrankutumov@gmail.com>
2020-06-23 11:24:36 +0300

    Lider Uslug Added

---
+++ demo/LiderUslug/js/main.js
@@ -0,0 +1,7 @@
+// Preloader
+$( function() {
+    var $preloader = $('#page-preloader'),
+        $spinner   = $preloader.find('.spinner');
+    $spinner.fadeOut();
+    $preloader.delay(350).fadeOut('slow');
+});
