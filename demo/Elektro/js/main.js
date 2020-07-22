e3735dbc20eaf9d0d9ce6926080031bd2e36de87
Cameron Class <kamrankutumov@gmail.com>
2020-07-22 15:08:34 +0300

    Elekrto

--- demo/Elektro/js/main.js
+++ demo/Elektro/js/main.js
@@ -1,19 +1,28 @@
 // Preloader
-$( function() {
-    var $preloader = $('#page-preloader'),
-        $spinner   = $preloader.find('.spinner');
-    $spinner.fadeOut();
-    $preloader.delay(350).fadeOut('slow');
+$(function () {
+  var $preloader = $("#page-preloader"),
+    $spinner = $preloader.find(".spinner");
+  $spinner.fadeOut();
+  $preloader.delay(350).fadeOut("slow");
 });
 
 // Slider
-var swiper = new Swiper('.swiper-container', {
+var swiper = new Swiper(".home-slider .swiper-container", {
   loop: true,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
+  },
+});
+
+var swiper = new Swiper(".brands .swiper-container", {
+  slidesPerView: 6,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
