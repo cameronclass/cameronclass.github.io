adb979524eed08f744e9f56c78280ec97e918fff
Cameron Class <kamrankutumov@gmail.com>
2020-07-23 11:46:54 +0300

    Elekro

--- demo/Elektro/js/main.js
+++ demo/Elektro/js/main.js
@@ -6,7 +6,31 @@ $(function () {
   $preloader.delay(350).fadeOut("slow");
 });
 
-// Slider
+
+
+/* Jquery */
+$(document).ready(function () {
+  /* Продукты Табы */
+  $(".product__link li:first-child").addClass("active");
+  $(".js-product-content").hide();
+  $(".js-product-content:first").show();
+
+  // Click function
+  $(".product__link li").click(function () {
+    $(".product__link li").removeClass("active");
+    $(this).addClass("active");
+    $(".js-product-content").hide();
+
+    var activeTab = $(this).find("a").attr("href");
+    $(activeTab).fadeIn();
+    return false;
+  });
+});
+
+
+
+
+/* Swiper JS */
 var swiper = new Swiper(".home-slider .swiper-container", {
   loop: true,
   navigation: {
