b0f7926adf478d2b65af96b25ca3f8063b5ab5a6
Cameron Class <kamrankutumov@gmail.com>
2020-07-25 01:26:02 +0300

    Tabs Finished

--- demo/OpenCart/js/main.js
+++ demo/OpenCart/js/main.js
@@ -6,6 +6,24 @@ $( function() {
     $preloader.delay(350).fadeOut('slow');
 });
 
+$(document).ready(function () {
+  /* Продукты Табы */
+  $(".choose-type__link li:first-child").addClass("active");
+  $(".js-product-content").hide();
+  $(".js-product-content:first").show();
+
+  // Click function
+  $(".choose-type__link li").click(function () {
+    $(".choose-type__link li").removeClass("active");
+    $(this).addClass("active");
+    $(".js-product-content").hide();
+
+    var activeTab = $(this).find("a").attr("href");
+    $(activeTab).fadeIn();
+    return false;
+  });
+});
+
 
 
 /* Slider */
@@ -14,6 +32,17 @@ var mySwiper = new Swiper(".home-slider .swiper-container", {
   effect: "fade",
   spaceBetween: 30,
 
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper(".video .swiper-container", {
+  slidesPerView: 2,
+  spaceBetween: 50,
+  loop: true,
+  centeredSlides: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
