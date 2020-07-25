8c7fa65ed801f646cb8dc1473463ac1d5fe0edbb
Cameron Class <kamrankutumov@gmail.com>
2020-07-25 19:04:40 +0300

    Marlin Php

---
+++ demo/MarlinPHP/create.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="ru">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="robots" content="index, follow">
+  <!-- MetaTags -->
+  <meta name="description" content="Description of the page less than 150 characters">
+  <meta name="keywords" content="KeyWords">
+  <!-- OG tags -->
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="https://example.com">
+  <meta property="og:title" content="Content Title">
+  <meta property="og:image" content="https://www.developmentlogics.com/wp-content/uploads/2017/07/4.jpg">
+  <meta property="og:description" content="Description Here">
+  <meta property="og:site_name" content="Site Name">
+  <meta property="og:locale" content="ru_RU">
+  <meta name="twitter:card" content="summary">
+  <meta name="twitter:title" content="Site Name">
+  <meta name="twitter:description" content="Description Here">
+  <meta name="twitter:image" content="https://www.developmentlogics.com/wp-content/uploads/2017/07/4.jpg">
+  <!-- Favivon -->
+  <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+  <link rel="icon" type="image/png" href="https://example.com/favicon.png">
+  <!-- Apple Touch Icon -->
+  <link rel="apple-touch-icon" href="/custom-icon.png">
+  <!-- Styles CSS -->
+  <link rel="stylesheet" href="css/libs.css">
+  <link rel="stylesheet" href="css/style.css">
+  <!-- Fonts and Icons-->
+  <title>Marlin PHP</title>
+</head>
+
+<body>
+  <!-- Preloader -->
+  <div id="page-preloader">
+    <div class="cssload-wraper">
+      <div class="cssload-dots"></div>
+    </div>
+  </div>
+  <!-- Header -->
+  <header class="container mb-5">
+    <h1>Добавления продукта</h1>
+  </header>
+  <!-- Main -->
+  <main class="container">
+    <div class="form-group mb-2">
+      <label for="my-input">Названия</label>
+      <input id="my-input" class="form-control" type="text" name="">
+    </div>
+     <div class="form-group mb-4">
+      <label for="my-input">Описания</label>
+      <textarea name="" id="" class="form-control">
+
+      </textarea>
+    </div>
+     <div class="form-group mb-5 d-flex flex-column">
+      <label for="my-input">Категория</label>
+      <select name="" id="">
+        <option value="1">Телефон</option>
+        <option value="1">Телефон</option>
+        <option value="1">Телефон</option>
+      </select>
+    </div>
+     <div class="form-group mb-5 d-flex flex-column">
+      <label for="my-input">Картинка</label>
+      <input type="file" name="" id="">
+    </div>
+     <div class="form-group mb-2">
+      <label for="my-input">Показывать</label>
+      <input type="checkbox" name="" id="">
+    </div>
+     <div class="form-group mb-2">
+      <button type="submit" class="btn btn-primary mt-3">Добавить</button>
+    </div>
+  </main>
+  <!-- Footer -->
+  <footer></footer>
+  <!-- Scripts -->
+  <script src="js/libs.min.js"></script>
+  <script src="js/main.js"></script>
+</body>
+
+</html>
