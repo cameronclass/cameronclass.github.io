92c0e11bb3de19e682292bd2574575d8c1c3b7dd
Cameron Class <kamrankutumov@gmail.com>
2020-08-04 21:36:45 +0300

    Elektro Home Page Finished

--- demo/Elektro/js/main.js
+++ demo/Elektro/js/main.js
@@ -50,6 +50,21 @@ $(document).ready(function () {
     $(this).toggleClass("active");
   });
 
+  /* Footer nav */
+  $(".footer__nav h5").click(function () {
+    $(this).parent().find("ul").toggleClass("d-none");
+  });
+
+  $(window)
+    .resize(function () {
+      if ($(window).width() < 600) {
+        $(".footer__nav ul").addClass("d-none");
+      } else {
+        $(".footer__nav ul").removeClass("d-none");
+      }
+    })
+    .resize();
+
   /* Insrese Value */
 
   $(".js-increase").on("click", function () {
@@ -66,6 +81,45 @@ $(document).ready(function () {
     if (!isNaN(currentVal) && currentVal > 1) {
       $qty.val(currentVal - 1);
     }
+  });
+
+  /* Catalog Readmore */
+  $(function () {
+    $(".js-calatog-readmore").readmore({
+      speed: 500,
+      lessLink:
+        '<div class="js-calatog-readmore-btn"><a href="#">Скрыть</a></div>',
+      moreLink:
+        '<div class="js-calatog-readmore-btn"><a href="#">Еще</a></div>',
+      collapsedHeight: 76,
+    });
+  });
+
+  /* Mobile Menu */
+  $(".js-mobile-menu-open").click(function () {
+    $(".header-mobile").addClass("mobile-open");
+    $(".header-mobile__overlay").addClass("d-block");
+    $("body").addClass("lock");
+  });
+  $(".header-mobile__overlay").click(function () {
+    $(".header-mobile").removeClass("mobile-open");
+    $(this).removeClass("d-block");
+    $("body").removeClass("lock");
+  });
+
+  $(".js-menu-expand").click(function (e) {
+    e.preventDefault();
+    $(this)
+      .parent()
+      .find(".header-mobile__menu_detail")
+      .addClass("menu-expand");
+  });
+  $(".js-menu-expand-close").click(function (e) {
+    e.preventDefault();
+    $(this)
+      .parents()
+      .find(".header-mobile__menu_detail")
+      .removeClass("menu-expand");
   });
 });
 
@@ -98,10 +152,18 @@ var swiper = new Swiper(".brands .swiper-container", {
       spaceBetween: 60,
     },
     850: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+    720: {
       slidesPerView: 3,
       spaceBetween: 20,
     },
     600: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    320: {
       slidesPerView: 2,
       spaceBetween: 20,
     },
@@ -115,5 +177,28 @@ var swiper = new Swiper(".watched-before .swiper-container", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    1300: {
+      slidesPerView: 5,
+      spaceBetween: 15,
+    },
+    990: {
+      slidesPerView: 4,
+      spaceBetween: 15,
+    },
+    850: {
+      slidesPerView: 3,
+      spaceBetween: 15,
+    },
+    720: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+    },
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 5,
+    },
   },
 });
