5e1964b0e787a8576491bd65d4dc03fce18e7bb4
Cameron Class <kamrankutumov@gmail.com>
2020-08-06 20:33:15 +0300

    Rent Car

--- demo/RentCar/js/main.js
+++ demo/RentCar/js/main.js
@@ -6,16 +6,94 @@
   $preloader.delay(350).fadeOut('slow');
 }); */
 
+
 $(document).ready(function () {
-          
+
+  /* Modal */
   $('.modal').modal();
 
+
+  /* Searchbar */
   $('input.autocomplete').autocomplete({
     data: {
       "Apple": null,
       "Microsoft": null,
-      "Google": 'https://placehold.it/250x250'
+      "Google": null,
     },
   });
 
+
+  /* Tabs */
+  $('.tabs').tabs();
+
+
+
+  /* Datapicker */
+
+  $(".datepicker").datepicker({
+    autoClose: true,
+    // internationalization
+    i18n: {
+      cancel: 'Cancel',
+      clear: 'Clear',
+      done: 'Ok',
+      previousMonth: '‹',
+      nextMonth: '›',
+      months: [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+      ],
+      monthsShort: [
+        'Jan',
+        'Feb',
+        'Mar',
+        'Apr',
+        'May',
+        'Jun',
+        'Jul',
+        'Aug',
+        'Sep',
+        'Oct',
+        'Nov',
+        'Dec'
+      ],
+      weekdays: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+      weekdaysShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+      weekdaysAbbrev: ['S', 'M', 'T', 'W', 'T', 'F', 'S']
+    },
+  });
+
+  /* $("#start").datepicker({
+    autoClose: true,
+    minDate: today,
+  });
+
+  $("#end").datepicker({
+    autoClose: true,
+  }); */
+
+
+
+
+
+  /* Timepicker */
+  $('.timepicker').timepicker({
+    autoClose: true,
+  });
+
+
+
+
+
 });
+
