cc669eb2289b49b92b5d813001bf386286e284bb
Cameron Class <kamrankutumov@gmail.com>
2020-09-01 21:10:19 +0300

    Other Pages Added

--- demo/Elektro/js/main.js
+++ demo/Elektro/js/main.js
@@ -191,6 +191,31 @@ $(document).ready(function () {
       collapsedHeight: 115,
     });
   });
+
+  /* Modal js-modal-phone-call js-modal-text*/
+  $("#modal-call").click(function (e) {
+    e.preventDefault();
+    $("#js-modal-phone-call").fadeIn("fast");
+    $("body").addClass("lock")
+  });
+
+  $("#modal-text").click(function (e) {
+    e.preventDefault();
+    $("#js-modal-text").fadeIn("fast");
+    $("body").addClass("lock")
+  });
+  
+  $(".modal__overlay").click(function (e) {
+    e.preventDefault();
+    $(".modal").fadeOut("fast");
+    $("body").removeClass("lock")
+  });
+  $(".modal__content_title .close").click(function (e) {
+    e.preventDefault();
+    $(".modal").fadeOut("fast");
+    $("body").removeClass("lock")
+  });
+
 });
 
 /* Swiper JS */
@@ -334,3 +359,31 @@ var galleryTop = new Swiper('.product__gallery .gallery-top', {
     swiper: galleryThumbs
   }
 });
+
+
+
+/* PLEASE DO NOT COPY AND PASTE THIS CODE. */
+(function () {
+  var w = window,
+    C = '___grecaptcha_cfg',
+    cfg = w[C] = w[C] || {},
+    N = 'grecaptcha';
+  var gr = w[N] = w[N] || {};
+  gr.ready = gr.ready || function (f) {
+    (cfg['fns'] = cfg['fns'] || []).push(f);
+  };
+  (cfg['render'] = cfg['render'] || []).push('onload');
+  w['__google_recaptcha_client'] = true;
+  var d = document,
+    po = d.createElement('script');
+  po.type = 'text/javascript';
+  po.async = true;
+  po.src = 'https://www.gstatic.com/recaptcha/releases/QVh-Tz10ahidjrORgXOS1oB0/recaptcha__ru.js';
+  var e = d.querySelector('script[nonce]'),
+    n = e && (e['nonce'] || e.getAttribute('nonce'));
+  if (n) {
+    po.setAttribute('nonce', n);
+  }
+  var s = d.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(po, s);
+})();
