364efaf5b83383e3d85f914ef250711cd3685801
Cameron Class <kamrankutumov@gmail.com>
2020-09-11 03:50:22 +0300

    Online School Base Added

--- demo/OnlineSchool/js/main.js
+++
@@ -1,7 +0,0 @@
-// Preloader
-$( function() {
-    var $preloader = $('#page-preloader'),
-        $spinner   = $preloader.find('.spinner');
-    $spinner.fadeOut();
-    $preloader.delay(350).fadeOut('slow');
-});
