6382ef543b3519ec29bf916c07b94c8f371c4bed
Cameron Class <kamrankutumov@gmail.com>
2020-09-18 17:47:25 +0300

    Gazeta

--- demo/Gazeta/js/main.js
+++ demo/Gazeta/js/main.js
@@ -1,16 +1,20 @@
 // Preloader
-$( function() {
-    var $preloader = $('#page-preloader'),
-        $spinner   = $preloader.find('.spinner');
-    $spinner.fadeOut();
-    $preloader.delay(350).fadeOut('slow');
+$(function () {
+  var $preloader = $('#page-preloader'),
+    $spinner = $preloader.find('.spinner');
+  $spinner.fadeOut();
+  $preloader.delay(350).fadeOut('slow');
 });
 
-
+// One Page Scroll
 $(document).ready(function () {
   $(".main").onepage_scroll({
-    sectionContainer: "section",
+    sectionContainer: ".scroll-section",
     responsiveFallback: 600,
-    loop: true
+    loop: false
   });
+  $(".next-section").click(function () {
+    $(".main").moveDown();
+  });
+
 });
