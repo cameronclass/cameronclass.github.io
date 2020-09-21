299926f2bc1bafa291fcd7327c44975be8fdd95a
Cameron Class <kamrankutumov@gmail.com>
2020-09-21 20:03:29 +0300

    Second page

--- demo/Uikit-Work/js/main.js
+++ demo/Uikit-Work/js/main.js
@@ -13,3 +13,65 @@ $(function () {
     $(".hamburger").removeClass("is-active");
   }); */
 });
+
+
+/* Tests */
+/*  */
+  $('#quiz').quiz({
+    counterFormat: 'Вопрос %current из %total',
+    resultsFormat: "<img src=img/finish-quiz.png>Поздравляем! Вы заработали %score Баллов. <br> Следите за турнирной таблицей!",
+    questions: [{
+        'q': 'A smaple question?',
+        'options': [
+          'Answer 1',
+          'Answer 2',
+          'Answer 3',
+          'Answer 4'
+        ],
+        'correctIndex': 1,
+      },
+      {
+        'q': 'A smaple question?',
+        'options': [
+          'Answer 1',
+          'Answer 2'
+        ],
+        'correctIndex': 1,
+
+      },
+      {
+        'q': 'A smaple question?',
+        'options': [
+          'Answer 1',
+          'Answer 2',
+          'Answer 3',
+          'Answer 4'
+        ],
+        'correctIndex': 2,
+
+      },
+      {
+        'q': 'A smaple question?',
+        'options': [
+          'Answer 1',
+          'Answer 2'
+        ],
+        'correctIndex': 1,
+
+      },
+      {
+        'q': 'A smaple question?',
+        'options': [
+          'Answer 1',
+          'Answer 2',
+          'Answer 3',
+          'Answer 4'
+        ],
+        'correctIndex': 3,
+
+      },
+    ],
+    nextButtonText: "Следующий вопрос",
+    finishButtonText: "Закончить",
+    restartButtonText: "Закрыть",
+  }); 
