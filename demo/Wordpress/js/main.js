c3ebde06470207cabdd67dc867ccf8305587aa72
Cameron Class <kamrankutumov@gmail.com>
2020-10-07 04:03:17 +0300

    Home page finished

--- demo/Wordpress/js/main.js
+++ demo/Wordpress/js/main.js
@@ -1,12 +1,24 @@
 // Preloader
-$( function() {
-    var $preloader = $('#page-preloader'),
-        $spinner   = $preloader.find('.spinner');
-    $spinner.fadeOut();
-    $preloader.delay(350).fadeOut('slow');
+$(function () {
+  var $preloader = $("#page-preloader"),
+    $spinner = $preloader.find(".spinner");
+  $spinner.fadeOut();
+  $preloader.delay(350).fadeOut("slow");
 
-    /* Select */
-    $('.theme-select option[disabled="true"]').each(function () {
-      $(this).attr("style", "color:red");
-    });
+  /* Header */
+  /* $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 10) {
+      $("header").addClass("header-fixed");
+    } else {
+      $("header").removeClass("header-fixed");
+    }
+  }); */
+  /* Hamburger */
+  $(".hamburger").click(function () {
+    $(this).toggleClass("is-active");
+    $(".header__menu").toggleClass("header__menu_active");
+    $("html").toggleClass("lock");
+  });
 });
