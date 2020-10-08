d659c67737be0da2641568cd5871b785db8330d1
Cameron Class <kamrankutumov@gmail.com>
2020-10-08 20:32:48 +0300

    Change

--- demo/Wordpress/js/main.js
+++ demo/Wordpress/js/main.js
@@ -21,24 +21,77 @@ $(function () {
     $(".header__menu").toggleClass("header__menu_active");
     $("html").toggleClass("lock");
   });
+});
+function inputValidate(selector, that) {
+  if (
+    $(that)
+      .closest("#form-calculate")
+      .find("" + selector + "")
+      .val() == ""
+  ) {
+    $(that)
+      .closest("#form-calculate")
+      .find("" + selector + "")
+      .css("border", "1px solid red");
+    return false;
+  } else {
+    $(that)
+      .closest("#form-calculate")
+      .find("" + selector + "")
+      .css("border", "1px solid green");
+    return true;
+  }
+}
 
-  $('a[href="#contact"]').click(function (event) {
-    event.preventDefault();
-    $("#modal").modal({
-      fadeDuration: 250,
-    });
-  });
+function selectValidate(selector, that) {
+  if (
+    $(that)
+      .closest("#form-calculate")
+      .find("" + selector + "")
+      .val() == ""
+  ) {
+    $(that)
+      .closest("#form-calculate")
+      .find("" + selector + "")
+      .css("border", "1px solid red");
+    return false;
+  } else {
+    $(that)
+      .closest("#form-calculate")
+      .find("" + selector + "")
+      .css("border", "1px solid green");
+    return true;
+  }
+}
+
+// this will run on every select value change. if you want it to only run for those specific selects, add the same class in all of them and change the selector to $('select.yourclass')
+
+$(".js-btn").on("click", function () {
+  $(".mylabel").text(
+    parseInt($("select[name=format]").val()) +
+      parseInt($("input[name=days]").val()) *
+        parseInt($("input[name=hours]").val()) *
+        (parseInt($("input[name=pounts]").val()) / 2)
+    /* parseInt($("select[name=parametr]").val()) + */
+  );
 });
 
+$("#form-calculate").submit(function (e) {
+  e.preventDefault();
 
-map = new ymaps.Map(id, {
-  center: center,
-  zoom: 13,
+  if (
+    inputValidate("#days", $(this)) &&
+    inputValidate("#hours", $(this)) &&
+    inputValidate("#format", $(this)) &&
+    inputValidate("#pounts", $(this))
+  ) {
+    console.log("Hello");
+
+    $(".js-calc-modal").addClass("js-modal-open");
+  }
 });
-map.controls.remove("geolocationControl");
-map.controls.remove("searchControl");
-map.controls.remove("trafficControl");
-map.controls.remove("typeSelector");
-map.controls.remove("fullscreenControl");
-map.controls.remove("rulerControl");
-map.behaviors.disable(["scrollZoom"]);
+/*  js-modal-open */
+$(".js-calc-modal__modal .close-modal").click(function (e) {
+  e.preventDefault();
+  $(".js-calc-modal").removeClass("js-modal-open");
+});
