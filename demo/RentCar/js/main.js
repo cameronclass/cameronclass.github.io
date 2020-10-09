02695d17ddba868380754a0c8d30462deb084de2
Cameron Class <kamrankutumov@gmail.com>
2020-10-09 18:17:55 +0300

    Last changes

--- demo/RentCar/js/main.js
+++ demo/RentCar/js/main.js
@@ -6,7 +6,6 @@ $(function () {
   $preloader.delay(350).fadeOut("slow");
 });
 
-
 $(document).ready(function () {
   /* Menu Open */
   $(".js-menu-open").click(function () {
@@ -22,14 +21,22 @@ $(document).ready(function () {
     );
   });
 
+  $(".js-menu-close").click(function () {
+    $(".header__mobile-menu_overlay").removeClass("d-block");
+    $(".header__mobile-menu_navigation").removeClass(
+      "header__mobile-menu_navigation_active"
+    );
+  });
+
+  $(".js-disable").click(function (e) {
+    e.preventDefault();
+  });
+
   /* Modal */
   $(".modal").modal();
 
   /* Dropdown */
-  $(".dropdown-trigger").dropdown(
-
-  );
-
+  $(".dropdown-trigger").dropdown();
 
   /* Select */
   $("select").formSelect();
@@ -112,171 +119,174 @@ $(document).ready(function () {
     twelveHour: false,
   });
 
-
   /* Mask Input */
   $("#date-birth").mask("99 / 99 / 9999");
 
-
   /* Accordion */
 
-  var allPanels = $('.accordion > dd').hide();
+  var allPanels = $(".accordion > dd").hide();
 
-  $('.accordion > dt > a').click(function () {
+  $(".accordion > dt > a").click(function () {
     $this = $(this);
     $target = $this.parent().next();
 
-    if (!$target.hasClass('active')) {
-      allPanels.removeClass('active').slideUp().delay(50);
-      $target.addClass('active').slideDown().delay(50);
+    if (!$target.hasClass("active")) {
+      allPanels.removeClass("active").slideUp().delay(50);
+      $target.addClass("active").slideDown().delay(50);
     }
 
     return false;
   });
 
-
   /* Tooltip */
 
-  $('.tooltipped').tooltip();
+  $(".tooltipped").tooltip();
 
   /* Data Child Seat */
 
-  let allPrice = parseFloat($('[data-all-price]').attr('data-all-price'));
-  let childSeatPrice = parseFloat($('[data-child-seat-price]').attr('data-child-seat-price'));
-  let exstraDriverPrice = parseFloat($('[data-exstra-driver-price]').attr('data-exstra-driver-price'));
+  let allPrice = parseFloat($("[data-all-price]").attr("data-all-price"));
+  let childSeatPrice = parseFloat(
+    $("[data-child-seat-price]").attr("data-child-seat-price")
+  );
+  let exstraDriverPrice = parseFloat(
+    $("[data-exstra-driver-price]").attr("data-exstra-driver-price")
+  );
 
-  $('[data-all-price]').text(allPrice.toFixed(2));
-  $('[data-child-seat-price]').text(childSeatPrice.toFixed(2));
-  $('[data-exstra-driver-price]').text(exstraDriverPrice.toFixed(2));
+  $("[data-all-price]").text(allPrice.toFixed(2));
+  $("[data-child-seat-price]").text(childSeatPrice.toFixed(2));
+  $("[data-exstra-driver-price]").text(exstraDriverPrice.toFixed(2));
 
   $("[data-navigation]").change(function () {
-    let navigationPrice = parseFloat($(this).data('navigation'));
+    let navigationPrice = parseFloat($(this).data("navigation"));
     let quantity = null;
-    if ($(this).prop('checked')) {
-      allPrice = parseFloat($('[data-all-price]').attr('data-all-price'));
+    if ($(this).prop("checked")) {
+      allPrice = parseFloat($("[data-all-price]").attr("data-all-price"));
       quantity = allPrice + navigationPrice;
-      $('[data-all-price]').text(quantity.toFixed(2));
-      $('[data-all-price]').attr('data-all-price', quantity);
+      $("[data-all-price]").text(quantity.toFixed(2));
+      $("[data-all-price]").attr("data-all-price", quantity);
       return true;
     } else {
-      allPrice = parseFloat($('[data-all-price]').attr('data-all-price'));
+      allPrice = parseFloat($("[data-all-price]").attr("data-all-price"));
       quantity = allPrice - navigationPrice;
 
-      $('[data-all-price]').text(quantity.toFixed(2));
-      $('[data-all-price]').attr('data-all-price', quantity);
+      $("[data-all-price]").text(quantity.toFixed(2));
+      $("[data-all-price]").attr("data-all-price", quantity);
       return false;
-
     }
-
   });
   /* Child Seat */
-  $('[data-child-seat-count]').change(function () {
-    let seatCount = parseFloat($(this).attr('data-child-seat-count'));
+  $("[data-child-seat-count]").change(function () {
+    let seatCount = parseFloat($(this).attr("data-child-seat-count"));
     let quantity = null;
 
-    allPrice = parseFloat($('[data-all-price]').attr('data-all-price'));
+    allPrice = parseFloat($("[data-all-price]").attr("data-all-price"));
 
     console.log(seatCount);
-    if ($(this).prop('checked') && seatCount == 1) {
-      if ($('[data-old-count-seat]').attr('data-old-count-seat') == 2) {
-        allPrice = parseFloat($('[data-all-price]').attr('data-all-price')) - childSeatPrice * 2;
-        $('[data-all-price]').attr('data-all-price', allPrice);
-        $('[data-all-price]').text(allPrice.toFixed(2));
-
+    if ($(this).prop("checked") && seatCount == 1) {
+      if ($("[data-old-count-seat]").attr("data-old-count-seat") == 2) {
+        allPrice =
+          parseFloat($("[data-all-price]").attr("data-all-price")) -
+          childSeatPrice * 2;
+        $("[data-all-price]").attr("data-all-price", allPrice);
+        $("[data-all-price]").text(allPrice.toFixed(2));
       }
-      quantity = parseFloat($('[data-all-price]').attr('data-all-price')) + seatCount * childSeatPrice;
-      $('[data-old-count-seat]').attr('data-old-count-seat', seatCount);
-      $('[data-all-price]').attr('data-all-price', quantity);
-      $('[data-all-price]').text(quantity.toFixed(2));
+      quantity =
+        parseFloat($("[data-all-price]").attr("data-all-price")) +
+        seatCount * childSeatPrice;
+      $("[data-old-count-seat]").attr("data-old-count-seat", seatCount);
+      $("[data-all-price]").attr("data-all-price", quantity);
+      $("[data-all-price]").text(quantity.toFixed(2));
+    } else if ($(this).prop("checked") && seatCount == 2) {
+      if ($("[data-old-count-seat]").attr("data-old-count-seat") == 1) {
+        allPrice =
+          parseFloat($("[data-all-price]").attr("data-all-price")) -
+          childSeatPrice * 1;
+        $("[data-all-price]").attr("data-all-price", allPrice);
 
-
-
-    } else if ($(this).prop('checked') && seatCount == 2) {
-
-      if ($('[data-old-count-seat]').attr('data-old-count-seat') == 1) {
-        allPrice = parseFloat($('[data-all-price]').attr('data-all-price')) - childSeatPrice * 1;
-        $('[data-all-price]').attr('data-all-price', allPrice);
-
-        $('[data-all-price]').text(allPrice.toFixed(2));
-
-      }
-
-      quantity = parseFloat($('[data-all-price]').attr('data-all-price')) + seatCount * childSeatPrice;
-      $('[data-old-count-seat]').attr('data-old-count-seat', seatCount);
-      $('[data-all-price]').attr('data-all-price', quantity);
-      $('[data-all-price]').text(quantity.toFixed(2));
-
-    } else if ($(this).prop('checked') && seatCount == 0) {
-
-      if ($('[data-old-count-seat]').attr('data-old-count-seat') == 1) {
-        allPrice = parseFloat($('[data-all-price]').attr('data-all-price')) - childSeatPrice * 1;
-        $('[data-all-price]').attr('data-all-price', allPrice);
-        $('[data-all-price]').text(allPrice.toFixed(2));
-      }
-      if ($('[data-old-count-seat]').attr('data-old-count-seat') == 2) {
-        allPrice = parseFloat($('[data-all-price]').attr('data-all-price')) - childSeatPrice * 2;
-        $('[data-all-price]').attr('data-all-price', allPrice);
-        $('[data-all-price]').text(allPrice.toFixed(2));
-
+        $("[data-all-price]").text(allPrice.toFixed(2));
       }
 
-      $('[data-old-count-seat]').attr('data-old-count-seat', seatCount);
+      quantity =
+        parseFloat($("[data-all-price]").attr("data-all-price")) +
+        seatCount * childSeatPrice;
+      $("[data-old-count-seat]").attr("data-old-count-seat", seatCount);
+      $("[data-all-price]").attr("data-all-price", quantity);
+      $("[data-all-price]").text(quantity.toFixed(2));
+    } else if ($(this).prop("checked") && seatCount == 0) {
+      if ($("[data-old-count-seat]").attr("data-old-count-seat") == 1) {
+        allPrice =
+          parseFloat($("[data-all-price]").attr("data-all-price")) -
+          childSeatPrice * 1;
+        $("[data-all-price]").attr("data-all-price", allPrice);
+        $("[data-all-price]").text(allPrice.toFixed(2));
+      }
+      if ($("[data-old-count-seat]").attr("data-old-count-seat") == 2) {
+        allPrice =
+          parseFloat($("[data-all-price]").attr("data-all-price")) -
+          childSeatPrice * 2;
+        $("[data-all-price]").attr("data-all-price", allPrice);
+        $("[data-all-price]").text(allPrice.toFixed(2));
+      }
 
+      $("[data-old-count-seat]").attr("data-old-count-seat", seatCount);
     }
-
   });
 
   /* Exstra Driver */
-  $('[data-exstra-driver-count]').change(function () {
-    let driverCount = parseFloat($(this).attr('data-exstra-driver-count'));
+  $("[data-exstra-driver-count]").change(function () {
+    let driverCount = parseFloat($(this).attr("data-exstra-driver-count"));
     let quantity = null;
 
-    allPrice = parseFloat($('[data-all-price]').attr('data-all-price'));
+    allPrice = parseFloat($("[data-all-price]").attr("data-all-price"));
 
     console.log(driverCount);
-    if ($(this).prop('checked') && driverCount == 1) {
-      if ($('[data-old-exstra-price]').attr('data-old-exstra-price') == 2) {
-        allPrice = parseFloat($('[data-all-price]').attr('data-all-price')) - childSeatPrice * 2;
-        $('[data-all-price]').attr('data-all-price', allPrice);
-        $('[data-all-price]').text(allPrice.toFixed(2));
+    if ($(this).prop("checked") && driverCount == 1) {
+      if ($("[data-old-exstra-price]").attr("data-old-exstra-price") == 2) {
+        allPrice =
+          parseFloat($("[data-all-price]").attr("data-all-price")) -
+          childSeatPrice * 2;
+        $("[data-all-price]").attr("data-all-price", allPrice);
+        $("[data-all-price]").text(allPrice.toFixed(2));
       }
-      quantity = parseFloat($('[data-all-price]').attr('data-all-price')) + driverCount * childSeatPrice;
-      $('[data-old-exstra-price]').attr('data-old-exstra-price', driverCount);
-      $('[data-all-price]').attr('data-all-price', quantity);
-      $('[data-all-price]').text(quantity.toFixed(2));
+      quantity =
+        parseFloat($("[data-all-price]").attr("data-all-price")) +
+        driverCount * childSeatPrice;
+      $("[data-old-exstra-price]").attr("data-old-exstra-price", driverCount);
+      $("[data-all-price]").attr("data-all-price", quantity);
+      $("[data-all-price]").text(quantity.toFixed(2));
+    } else if ($(this).prop("checked") && driverCount == 2) {
+      if ($("[data-old-exstra-price]").attr("data-old-exstra-price") == 1) {
+        allPrice =
+          parseFloat($("[data-all-price]").attr("data-all-price")) -
+          childSeatPrice * 1;
+        $("[data-all-price]").attr("data-all-price", allPrice);
 
-    } else if ($(this).prop('checked') && driverCount == 2) {
-
-      if ($('[data-old-exstra-price]').attr('data-old-exstra-price') == 1) {
-        allPrice = parseFloat($('[data-all-price]').attr('data-all-price')) - childSeatPrice * 1;
-        $('[data-all-price]').attr('data-all-price', allPrice);
-
-        $('[data-all-price]').text(allPrice.toFixed(2));
-
-      }
-
-      quantity = parseFloat($('[data-all-price]').attr('data-all-price')) + driverCount * childSeatPrice;
-      $('[data-old-exstra-price]').attr('data-old-exstra-price', driverCount);
-      $('[data-all-price]').attr('data-all-price', quantity);
-      $('[data-all-price]').text(quantity.toFixed(2));
-
-    } else if ($(this).prop('checked') && driverCount == 0) {
-
-      if ($('[data-old-exstra-price]').attr('data-old-exstra-price') == 1) {
-        allPrice = parseFloat($('[data-all-price]').attr('data-all-price')) - childSeatPrice * 1;
-        $('[data-all-price]').attr('data-all-price', allPrice);
-        $('[data-all-price]').text(allPrice.toFixed(2));
-      }
-      if ($('[data-old-exstra-price]').attr('data-old-exstra-price') == 2) {
-        allPrice = parseFloat($('[data-all-price]').attr('data-all-price')) - childSeatPrice * 2;
-        $('[data-all-price]').attr('data-all-price', allPrice);
-        $('[data-all-price]').text(allPrice.toFixed(2));
-
+        $("[data-all-price]").text(allPrice.toFixed(2));
       }
 
-      $('[data-old-exstra-price]').attr('data-old-exstra-price', driverCount);
+      quantity =
+        parseFloat($("[data-all-price]").attr("data-all-price")) +
+        driverCount * childSeatPrice;
+      $("[data-old-exstra-price]").attr("data-old-exstra-price", driverCount);
+      $("[data-all-price]").attr("data-all-price", quantity);
+      $("[data-all-price]").text(quantity.toFixed(2));
+    } else if ($(this).prop("checked") && driverCount == 0) {
+      if ($("[data-old-exstra-price]").attr("data-old-exstra-price") == 1) {
+        allPrice =
+          parseFloat($("[data-all-price]").attr("data-all-price")) -
+          childSeatPrice * 1;
+        $("[data-all-price]").attr("data-all-price", allPrice);
+        $("[data-all-price]").text(allPrice.toFixed(2));
+      }
+      if ($("[data-old-exstra-price]").attr("data-old-exstra-price") == 2) {
+        allPrice =
+          parseFloat($("[data-all-price]").attr("data-all-price")) -
+          childSeatPrice * 2;
+        $("[data-all-price]").attr("data-all-price", allPrice);
+        $("[data-all-price]").text(allPrice.toFixed(2));
+      }
 
+      $("[data-old-exstra-price]").attr("data-old-exstra-price", driverCount);
     }
-
   });
-
 });
