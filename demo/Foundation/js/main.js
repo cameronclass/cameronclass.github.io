abf37c0f6f800d36ec88e54b6bbfcd49edfd8ea7
Cameron Class <kamrankutumov@gmail.com>
2020-10-13 23:15:48 +0300

    foundation

--- demo/Foundation/js/main.js
+++ demo/Foundation/js/main.js
@@ -1,7 +1,22 @@
 // Preloader
-$( function() {
+/* $( function() {
     var $preloader = $('#page-preloader'),
         $spinner   = $preloader.find('.spinner');
     $spinner.fadeOut();
     $preloader.delay(350).fadeOut('slow');
+}); */
+
+var swiper = new Swiper(".slider .swiper-container", {
+  /* cssMode: true, */
+  loop: true,
+  effect: "fade",
+  navigation: {
+    nextEl: ".slider .swiper-button-next",
+    prevEl: ".slider .swiper-button-prev",
+  },
+  pagination: {
+    el: ".slider .swiper-pagination",
+  },
+  clickable: true,
+  keyboard: true,
 });
