371d8a2bdb96a87c0a1340e17aabaf9a811beae2
Cameron Class <kamrankutumov@gmail.com>
2020-11-10 00:28:49 +0300

    dodopizza added

---
+++ demo/Dodoma/assets/js/main.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+  $('.phone').mask('+7 (999) 999-99-99');
+  // $('.date').mask('12.12.1111');
+});
---
+++ demo/LiderUslug/components/header-logged.php
@@ -0,0 +1,279 @@
+  <header class="container-fluid header header-logged">
+    <div class="container header__block">
+      <div class="header__logo">
+        <a class="category-open" href="#">
+          <img src="img/logo.png" alt="Логотип">
+        </a>
+        <a href="#">
+          <div class="logo-text">
+            <span>Лидер Услуг</span>
+            <div>Сервис заказа услуг</div>
+          </div>
+        </a>
+        <div class="header__category">
+          <div class="col-4">
+            <div class="col-4-title">
+              <img src="img/icons/category1.svg" alt="">
+              <h4>Домашний мастер</h4>
+            </div>
+            <ul class="js-category-readmore">
+              <li><a href="#">Сантехник</a></li>
+              <li><a href="#">Электрик</a></li>
+              <li><a href="#">Муж на час</a></li>
+              <li><a href="#">Столяр</a></li>
+              <li><a href="#">Слесарь</a></li>
+              <li><a href="#">Столяр</a></li>
+              <li><a href="#">Слесарь</a></li>
+            </ul>
+          </div>
+          <div class="col-4">
+            <div class="col-4-title">
+              <img src="img/icons/category1.svg" alt="">
+              <h4>Домашний мастер</h4>
+            </div>
+            <ul class="js-category-readmore">
+              <li><a href="#">Сантехник</a></li>
+              <li><a href="#">Электрик</a></li>
+              <li><a href="#">Муж на час</a></li>
+              <li><a href="#">Столяр</a></li>
+              <li><a href="#">Слесарь</a></li>
+              <li><a href="#">Столяр</a></li>
+              <li><a href="#">Слесарь</a></li>
+            </ul>
+          </div>
+          <div class="col-4">
+            <div class="col-4-title">
+              <img src="img/icons/category1.svg" alt="">
+              <h4>Домашний мастер</h4>
+            </div>
+            <ul class="js-category-readmore">
+              <li><a href="#">Сантехник</a></li>
+              <li><a href="#">Электрик</a></li>
+              <li><a href="#">Муж на час</a></li>
+              <li><a href="#">Столяр</a></li>
+              <li><a href="#">Слесарь</a></li>
+              <li><a href="#">Столяр</a></li>
+              <li><a href="#">Слесарь</a></li>
+            </ul>
+          </div>
+          <div class="col-4">
+            <div class="col-4-title">
+              <img src="img/icons/category1.svg" alt="">
+              <h4>Домашний мастер</h4>
+            </div>
+            <ul class="js-category-readmore">
+              <li><a href="#">Сантехник</a></li>
+              <li><a href="#">Электрик</a></li>
+              <li><a href="#">Муж на час</a></li>
+              <li><a href="#">Столяр</a></li>
+              <li><a href="#">Слесарь</a></li>
+              <li><a href="#">Столяр</a></li>
+              <li><a href="#">Слесарь</a></li>
+            </ul>
+          </div>
+          <div class="col-4">
+            <div class="col-4-title">
+              <img src="img/icons/category1.svg" alt="">
+              <h4>Домашний мастер</h4>
+            </div>
+            <ul class="js-category-readmore">
+              <li><a href="#">Сантехник</a></li>
+              <li><a href="#">Электрик</a></li>
+              <li><a href="#">Муж на час</a></li>
+              <li><a href="#">Столяр</a></li>
+              <li><a href="#">Слесарь</a></li>
+              <li><a href="#">Столяр</a></li>
+              <li><a href="#">Слесарь</a></li>
+            </ul>
+          </div>
+          <div class="col-4">
+            <div class="col-4-title">
+              <img src="img/icons/category1.svg" alt="">
+              <h4>Домашний мастер</h4>
+            </div>
+            <ul class="js-category-readmore">
+              <li><a href="#">Сантехник</a></li>
+              <li><a href="#">Электрик</a></li>
+              <li><a href="#">Муж на час</a></li>
+              <li><a href="#">Столяр</a></li>
+              <li><a href="#">Слесарь</a></li>
+              <li><a href="#">Столяр</a></li>
+              <li><a href="#">Слесарь</a></li>
+            </ul>
+          </div>
+          <div class="col-4">
+            <div class="col-4-title">
+              <img src="img/icons/category1.svg" alt="">
+              <h4>Домашний мастер</h4>
+            </div>
+            <ul class="js-category-readmore">
+              <li><a href="#">Сантехник</a></li>
+              <li><a href="#">Электрик</a></li>
+              <li><a href="#">Муж на час</a></li>
+              <li><a href="#">Столяр</a></li>
+              <li><a href="#">Слесарь</a></li>
+              <li><a href="#">Столяр</a></li>
+              <li><a href="#">Слесарь</a></li>
+            </ul>
+          </div>
+          <div class="col-4">
+            <div class="col-4-title">
+              <img src="img/icons/category1.svg" alt="">
+              <h4>Домашний мастер</h4>
+            </div>
+            <ul class="js-category-readmore">
+              <li><a href="#">Сантехник</a></li>
+              <li><a href="#">Электрик</a></li>
+              <li><a href="#">Муж на час</a></li>
+              <li><a href="#">Столяр</a></li>
+              <li><a href="#">Слесарь</a></li>
+              <li><a href="#">Столяр</a></li>
+              <li><a href="#">Слесарь</a></li>
+            </ul>
+          </div>
+          <div class="col-4">
+            <div class="col-4-title">
+              <img src="img/icons/category1.svg" alt="">
+              <h4>Домашний мастер</h4>
+            </div>
+            <ul class="js-category-readmore">
+              <li><a href="#">Сантехник</a></li>
+              <li><a href="#">Электрик</a></li>
+              <li><a href="#">Муж на час</a></li>
+              <li><a href="#">Столяр</a></li>
+              <li><a href="#">Слесарь</a></li>
+              <li><a href="#">Столяр</a></li>
+              <li><a href="#">Слесарь</a></li>
+            </ul>
+          </div>
+          <div class="col-4">
+            <div class="col-4-title">
+              <img src="img/icons/category1.svg" alt="">
+              <h4>Домашний мастер</h4>
+            </div>
+            <ul class="js-category-readmore">
+              <li><a href="#">Сантехник</a></li>
+              <li><a href="#">Электрик</a></li>
+              <li><a href="#">Муж на час</a></li>
+              <li><a href="#">Столяр</a></li>
+              <li><a href="#">Слесарь</a></li>
+              <li><a href="#">Столяр</a></li>
+              <li><a href="#">Слесарь</a></li>
+            </ul>
+          </div>
+          <div class="col-4">
+            <div class="col-4-title">
+              <img src="img/icons/category1.svg" alt="">
+              <h4>Домашний мастер</h4>
+            </div>
+            <ul class="js-category-readmore">
+              <li><a href="#">Сантехник</a></li>
+              <li><a href="#">Электрик</a></li>
+              <li><a href="#">Муж на час</a></li>
+              <li><a href="#">Столяр</a></li>
+              <li><a href="#">Слесарь</a></li>
+              <li><a href="#">Столяр</a></li>
+              <li><a href="#">Слесарь</a></li>
+            </ul>
+          </div>
+          <div class="col-4">
+            <div class="col-4-title">
+              <img src="img/icons/category1.svg" alt="">
+              <h4>Домашний мастер</h4>
+            </div>
+            <ul class="js-category-readmore">
+              <li><a href="#">Сантехник</a></li>
+              <li><a href="#">Электрик</a></li>
+              <li><a href="#">Муж на час</a></li>
+              <li><a href="#">Столяр</a></li>
+              <li><a href="#">Слесарь</a></li>
+              <li><a href="#">Столяр</a></li>
+              <li><a href="#">Слесарь</a></li>
+            </ul>
+          </div>
+          <div class="col-4">
+            <div class="col-4-title">
+              <img src="img/icons/category1.svg" alt="">
+              <h4>Домашний мастер</h4>
+            </div>
+            <ul class="js-category-readmore">
+              <li><a href="#">Сантехник</a></li>
+              <li><a href="#">Электрик</a></li>
+              <li><a href="#">Муж на час</a></li>
+              <li><a href="#">Столяр</a></li>
+              <li><a href="#">Слесарь</a></li>
+              <li><a href="#">Столяр</a></li>
+              <li><a href="#">Слесарь</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="header__actions">
+        <a class="btn header__btn header__btn_start" href="#">Разместить задание</a>
+        <a class="btn header__btn header__btn_quick" href="#">Быстрый заказ</a>
+      </div>
+      <form class="header__search" action="index.html" method="post">
+        <input class="header__search_input" type="text" name="" value="" placeholder="Что нужно сделать?">
+        <button class="btn header__search_btn" type="submit" name="button">Найти специалиста</button>
+      </form>
+      <div class="header__controls">
+        <button class="control-btn"><img src="img/icons/call.svg" alt=""> <span class="count">2</span>
+          <div class="dropdown-content">
+            <ul class="call">
+              <li><a href=v"#">+79991112233 <span class="subtitle">Viber whatsapp</span></a></li>
+              <li><a href="#">+79991112233 <span class="subtitle">Viber whatsapp</span></a></li>
+            </ul>
+          </div>
+        </button>
+        <button class="control-btn"><img src="img/icons/heart.svg" alt=""> <span class="count">2</span>
+          <div class="dropdown-content">
+            <ul class="notification">
+              <li><a href="#">Новых заданий <span class="count">1</span></a></li>
+              <li><a href="#">Предложение услуг <span class="count">0</span></a></li>
+            </ul>
+          </div>
+        </button>
+        <a href="#" class="control-btn"><img src="img/icons/email.svg" alt="">
+        </a>
+        <button class="control-btn"><img src="img/icons/question.svg" alt=""> </button>
+      </div>
+      <div class="header__login">
+        <button class="control-btn price">1500 ₽
+          <div class="dropdown-content">
+            <ul class="price-ul">
+              <li class="price-title">Баланс</li>
+              <li class="balance">
+                <span>Доступно</span>
+                <span>1500 ₽</span>
+              </li>
+              <li class="balance">
+                <span>Заблокировано</span>
+                <span>1500 ₽</span>
+              </li>
+              <li class="link"><a href="#">Вывод средств</a></li>
+              <li class="btn-action"><a href="#">Пополнить баланс</a></li>
+            </ul>
+          </div>
+        </button>
+        <div class="header__login_logged control-btn">
+          <img src="img/master-image.png" alt="">
+          <div class="header__login_name">
+            <h4>Александр Р...</h4>
+            <span>личный кабинет</span>
+          </div>
+          <div class="dropdown-content">
+            <ul class="master">
+              <li><a href="#">Мои заказы</a></li>
+              <li><a href="#">Избранное</a></li>
+              <li><a href="#">Сообщения</a></li>
+              <li><a href="#">Уведомления</a></li>
+              <hr>
+              <li><a href="#">Настройка</a></li>
+              <hr>
+              <li><a href="#">Выйти</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
